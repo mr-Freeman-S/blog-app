e51ae5cf2ced34529e81c7c85706433bb39cbc6a
mr-Freeman-S <mr.baranok.slava@gmail.com>
2023-05-20 20:38:57 +0300

    feat: add header menu with links and profile info

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import {HeaderMenu} from './pagesView/HeaderMenu';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <HeaderMenu/>
     </div>
   );
 }
---
+++ src/pagesView/HeaderMenu.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Container, Nav, Navbar, Image} from 'react-bootstrap';
+import profileImg from './../assets/img/profileImg.png'
+
+export const HeaderMenu = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand={false}>
+        <Container fluid>
+          <Navbar.Toggle/>
+          <Navbar.Collapse>
+            <Nav>
+              <Nav.Link href='#'>Posts</Nav.Link>
+              <Nav.Link href='#'>About Me</Nav.Link>
+            </Nav>
+            <Nav>
+              <Image src={profileImg} roundedCircle width={100} height={100}></Image>
+              <h3>Slava</h3>
+              <Nav.Link href='mailto:mr.baranok.slava@gmail.com'>mr.baranok.slava@gmail.com</Nav.Link>
+            </Nav>
+
+          </Navbar.Collapse>
+
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
